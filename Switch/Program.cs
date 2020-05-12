c430817d0b5f25cf7f3061f851632dced78ed2d0
Rajesh Bathula <greeshma-sadineni.com>
2020-05-12 15:35:30 +0200

    first commit

---
+++ Switch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num == 9 || num == 10)
+
+                Console.WriteLine("Excellent");
+            
+            else if (num == 7|| num ==8)
+                Console.WriteLine(" Notable");
+            else if (num == 6)
+                Console.WriteLine("Good");
+            else if (num == 0|| num == 1||num ==2 ||num == 3 ||num == 4)
+                Console.WriteLine("Fail");
+            else
+                Console.WriteLine("Pass");
+
+
+
+        }
+    }
+}
